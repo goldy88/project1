8d0dd38c8dade29d2d66dbc2e7df2fefd1156823
goldy88 <goldmann.daniel.88@gmail.com>
2020-07-14 10:49:22 +0200

    Initial commit

---
+++ src/com/goldmann/Main.java
@@ -0,0 +1,9 @@
+package com.goldmann;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------------------------------");
+        
+    }
+}
